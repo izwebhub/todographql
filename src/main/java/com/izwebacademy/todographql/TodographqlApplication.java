2e89d5a04b0d7a347823a7975c40fe2df8396ba6
developer <developer1@example.com>
2020-12-10 12:58:51 +0300

    first commit

---
+++ src/main/java/com/izwebacademy/todographql/TodographqlApplication.java
@@ -0,0 +1,13 @@
+package com.izwebacademy.todographql;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TodographqlApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TodographqlApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/izwebacademy/todographql/TodographqlApplicationTests.java
@@ -0,0 +1,13 @@
+package com.izwebacademy.todographql;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TodographqlApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
